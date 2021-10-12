b70d403dd47825d17e10d4f412ab08e19a472f6d
lengyelluk <lengyel.luk@gmail.com>
2021-10-12 23:01:47 +0200

    zmena textu prvy nastrel

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -4,24 +4,41 @@ import { Section } from '../layout/Section';
 const VerticalFeatures = () => (
   <Section
     title="Slovenská digitálna koruna"
-    description="Kopček zmrzliny stál jednu korunu. Teraz stojí jedno euro. Nenechaj si ukradnúť ešte viac! Mysli na svoju budúcnosť a budúcnosť svojich detí."
+    description="Kým dočítate tento odsek, 100 tisíc eur práve stratilo 1 cent zo svojej hodnoty.  
+    Pre 1 centovú medenú mincu sa bežný Slovák ani nezohne, prečo teda o nej píšeme? 
+    Za 12 rokov vďaka centovým stratám prídete o polovicu svojho majetku (bude link). 
+    Váš vysnívaný dom na ktorý roky sporíte bude polovičný (čo znamená, že si dom nikdy nepostavíte)."
   >
     <VerticalFeatureRow
-      title="Tvoj majetok klesá každým dňom"
-      description="Máš prácu aby si zabezpečil svoju rodinu a aj tak si môžeš dovoliť každý mesiac menej? Už si premýšľal ako je možné, že ceny na Slovensku rastú rýchlejšie ako platy?"
+      title="Práca, peniaze a ich hodnota"
+      description="O výške Vašej výplaty rozhoduje Vaša kvalifikácia, zručnosti a skúsenosti. 
+      Najvyššie platové ohodnotenie dosiahnete, ak budete mat kvalifikáciu v oblasti, 
+      ktorú neovláda až toľko ľudí. Trhový mechanizmus založený na dopyte a ponuke Vás potom férovo ohodnotí. 
+      Ale kvôli euru nie až tak férovo ako by ste si zaslúžili... hodnota 1 eura môže byť zajtra nižšia ako dnes."
       image="/assets/images/feature.svg"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
       title="Európska centrálna banka (ECB)"
-      description="Vieš kto stojí na jej čele? Christine Lagardová. Počul si už o nej? Myslíš, že ju volili slovenskí občania a má vôbec záujem o prosperitu Slovenska? Vieš, že ECB tlačí Eurá ako na bežiacom páse?"
+      description="O tom, akú hodnotu bude mať 1 euro nerozhoduje trh, tak ako o Vašej mzde, 
+      ale Rada guvernérov Európskej centrálnej banky (ECB). (bude Link: https://www.ecb.europa.eu/ecb/orga/decisions/govc/html/index.sk.html). 
+      o menovej politike  eurozóny, a teda aj o hodnote euromince, rozhoduje Šesť členov Výkonnej rady a guvernéri národných centrálnych bánk 19 krajín, hlasovaním. 
+      Guvernéri sa pri hlasovani rozhodujú podľa odporúčaní a modelov, ktoré vypracovalo 3500 odborníkov ECB. 
+      Nikoho ale neprekvapí, že všetky krajiny hája svoje individuálne záujmy a nehlasujú podľa odporúčaní odborníkov. 
+      Je samozrejmé, že bohaté krajiny ako Nemecko chcú chrániť svoje úspory. Už menej zrejmé je, že zadĺženým južanským štátom vyhovuje pokles hodnoty spoločnej európskej meny. 
+      S klesajúcou hodnotou eura sa znižujú ich (nesplatiteľné) dlhy bez toho, aby si občania v týchto krajinách museli utiahnuť opasky.
+      Hlasovanie je založené na základe protichodných ekonomických a politických záujmov krajín a osobných ambícií samotných guvernérov. 
+      Jednoducho povedané, cena peňazí nie je výsledkom snahy o najlepšie možné riešenie."
       image="/assets/images/feature2.svg"
       imageAlt="Second feature alt text"
       reverse
     />
     <VerticalFeatureRow
-      title="Euro je hrozba, riešenie je Slovenská digitálna koruna"
-      description="Zbav sa Eura, investuj do Slovenskej digitálnej koruny. So Slovenskou digitálnou korunou sa už nemusíš báť prepadu Eura. Držme a budujme náš spoločný kapitál na Slovensku."
+      title="Slovensko, euro a Slovenská digitálna koruna"
+      description="Záujmy 5 miliónov Slovákov sú na chvoste 340 miliónej menovej únie. Pre tých ktorí sa nechcú prizerať ako ich eurové úspory strácajú na hodnote prinášame riešenie.  
+      Investujte do Slovenskej digitálnej koruny, ktorá ochráni Vaše úspory pred rozhodnutiami (hlasovaním) Rady guvernérov ECB (kde dokonca guvernér Slovenskej národnej banky nemá tri mesiace v roku hlasovacie právo). Ponuka Slovenských digitálnych korún je konečná, nebude možné aby novovytvorené (vytlačené) peniaze znížili hodnotu Vašich príjmov alebo úspor. Naopak, rastúci dopyt bude viesť k rastu hodnoty Vašich investícii do Slovenskej digitálnej korune. 
+      Cena Slovenskej digitálnej koruny sa rovnako ako Vaša mzda odvíja od trhového mechanizmu dopytu a ponuky. Slovenská digitálna koruna nevzniká mimo územia Slovenska, nehlasuje sa o jej hodnote a komerčné banky nemajú na ňu žiadny vplyv. Mnohé vyspelé ekonomiky Európy (Dánsko alebo Švédsko) odmietli princíp umelo ovplyvňovanej meny zo zahraničia. 
+      Pridajte sa k investorom Slovenskej digitálnej koruny a získajte tak rovnakú možnosť úspešne ochrániť hodnotu svojich peňazí."
       image="/assets/images/feature3.svg"
       imageAlt="Third feature alt text"
     />
